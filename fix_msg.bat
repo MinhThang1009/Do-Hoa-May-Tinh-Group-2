e2693975667d30b58a198f4e09c2d6390abc1e01
MinhThang1009 <MinhThang1009@users.noreply.github.com>
2026-04-20 03:46:01 +0700

    Thiết lập tường lửa Gitignore chặn file đệm Vite và dọn sạch hệ thống

--- fix_msg.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-node c:/Users/Admin/Downloads/Do-Hoa-May-Tinh-Group-2-1/msg_editor.js %1
--- fix_seq.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-node c:/Users/Admin/Downloads/Do-Hoa-May-Tinh-Group-2-1/seq_editor.js %1
